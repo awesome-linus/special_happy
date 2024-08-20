35a8c619611c80e1aa20cdb47d75dbe159d3ef4f
kou <kou@awesome-tech-k.com>
2024-08-20 11:12:24 +0900

    add command

--- lib/special_happy/cli.rb
+++ lib/special_happy/cli.rb
@@ -7,5 +7,9 @@ module SpecialHappy
     def red(word) # コマンドはメソッドとして定義する
       say(word, :red)
     end
+    desc "blue WORD", "blue words print." # コマンドの使用例と、概要
+    def blue(word) # コマンドはメソッドとして定義する
+      say(word, :blue)
+    end
   end
 end
